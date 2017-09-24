d58a4dfeb2396bf177c1e7ca9ef31efc2623200e
daiiz <da.iz.mg@gmail.com>
2017-09-24 22:27:03 +0900

    build

--- dist/index.js
+++ dist/index.js
@@ -14,29 +14,32 @@ var HTMLNodesInArea = function () {
 
     this.targetNode = targetNode;
     this.TEXT = 3;
-    this.mark = Math.floor(Math.random() * 100000);
+    this.mark = Math.floor(Math.random() * 10000);
     this.range = null;
     this.nodes = [];
     this.result = [];
     this.tagName = this.TEXT;
-
-    // Settings
-    this.maxDepth = 20;
-    this.excludeTagNames = ['meta', 'script', 'style', 'iframe', 'audio', 'video'];
-    this.excludeTagIds = [];
-    this.excludeTagClassNames = [];
-    this.excludeInvisibles = true;
-    this.minStrLength = 1;
     this.detail = false;
+
+    // Options
+    this.options = {
+      maxDepth: 20,
+      excludeTagIds: [],
+      excludeTagClassNames: [],
+      excludeTagNames: ['meta', 'script', 'style', 'iframe', 'audio', 'video'],
+      excludeInvisibles: true,
+      minStrLength: 1,
+      onlyInTopLayer: true
+    };
   }
 
   _createClass(HTMLNodesInArea, [{
-    key: 'config',
-    value: function config() {
+    key: 'setOptions',
+    value: function setOptions() {
       var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
       for (var key in options) {
-        this[key] = options[key];
+        this.options[key] = options[key];
       }
     }
   }, {
@@ -48,12 +51,12 @@ var HTMLNodesInArea = function () {
       for (var i = 0; i < elems.length; i++) {
         var elem = elems[i];
         var isExclude = false;
-        if (this.excludeTagNames.indexOf(elem.nodeName.toLowerCase()) === -1) {
-          if (this.excludeTagIds.indexOf(elem.id) !== -1) isExclude = true;
+        if (this.options.excludeTagNames.indexOf(elem.nodeName.toLowerCase()) === -1) {
+          if (this.options.excludeTagIds.indexOf(elem.id) !== -1) isExclude = true;
           var classList = elem.classList;
           if (classList) {
             classList.forEach(function (cn) {
-              if (_this.excludeTagClassNames.indexOf(cn) !== -1) {
+              if (_this.options.excludeTagClassNames.indexOf(cn) !== -1) {
                 isExclude = true;
               }
             });
@@ -73,7 +76,7 @@ var HTMLNodesInArea = function () {
       }
 
       if (node.nodeType === 3) return;
-      if (dep > this.maxDepth) return;
+      if (dep > this.options.maxDepth) return;
       node.dataset.daiiz_visit = mark;
 
       var childNodes = this._filterElems(node.childNodes);
@@ -89,7 +92,7 @@ var HTMLNodesInArea = function () {
     key: '_register',
     value: function _register(node) {
       if (this.tagName === this.TEXT) {
-        if (node.nodeValue.trim().length >= this.minStrLength) {
+        if (node.nodeValue.trim().length >= this.options.minStrLength) {
           // textNodeに対してgetBoundingClientRectを実行できないのでspanでwrapする
           var p = node.parentNode;
           if (p.className !== 'lemonpie_text_span') {
@@ -118,6 +121,38 @@ var HTMLNodesInArea = function () {
       return false;
     }
   }, {
+    key: '_isTheTopLayer',
+    value: function _isTheTopLayer(node) {
+      var points = [[node.position.left + 1, node.position.top + 1], [node.position.right - 1, node.position.top + 1], [node.position.right - 1, node.position.bottom - 1], [node.position.left + 1, node.position.bottom - 1]];
+      var _iteratorNormalCompletion = true;
+      var _didIteratorError = false;
+      var _iteratorError = undefined;
+
+      try {
+        for (var _iterator = points[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+          var point = _step.value;
+
+          var elem = document.elementFromPoint(point[0] - window.pageXOffset, point[1] - window.pageYOffset);
+          if (elem === node.self || $(elem).find(node.self).length > 0 || $(node.self).find(elem).length > 0) return true;
+        }
+      } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+      } finally {
+        try {
+          if (!_iteratorNormalCompletion && _iterator.return) {
+            _iterator.return();
+          }
+        } finally {
+          if (_didIteratorError) {
+            throw _iteratorError;
+          }
+        }
+      }
+
+      return false;
+    }
+  }, {
     key: 'extract',
     value: function extract() {
       var range = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
@@ -135,7 +170,7 @@ var HTMLNodesInArea = function () {
       for (var i = 0; i < this.nodes.length; i++) {
         var textSpan = this.nodes[i];
         var rect = textSpan.getBoundingClientRect();
-        if (this.excludeInvisibles && rect.top === 0 && rect.bottom === 0 && rect.left === 0 && rect.right === 0) {
+        if (this.options.excludeInvisibles && rect.top === 0 && rect.bottom === 0 && rect.left === 0 && rect.right === 0) {
           continue;
         }
         var res = {
@@ -151,9 +186,12 @@ var HTMLNodesInArea = function () {
           res.self = textSpan;
           res.parent = textSpan.parentNode;
         }
-        if (this._isInvolvedIn(res.position)) {
-          this.result.push(res);
-        }
+
+        // 範囲内に存在しないものを無視
+        if (!this._isInvolvedIn(res.position)) continue;
+        // 最上層に存在しないものを無視
+        if (this.options.onlyInTopLayer && !this._isTheTopLayer(res)) continue;
+        this.result.push(res);
       }
       return this.result;
     }
